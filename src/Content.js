a2c0a6e6d060034db0830dd01a2543e5c103da26
michael tamsil <michaeltamsil@gmail.com>
2018-12-27 11:27:49 +0700

    update react

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ const Content = (props) => {
     return (
         <div>
             <Jumbotron>
-                <h1 className="display-3">my Name is xxx</h1>
+                <h1 className="display-3">my Name is Michael Tamsil</h1>
                 <p className="lead">I'm website maker in Batam</p>
                 <p>will give you chances to new and loyal customers loyal by your service through website</p>
                 <p className="lead"><a href="#contact">Contact me</a></p>
