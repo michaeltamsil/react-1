fd5a653f90bf606f5f4b3dafed98da66a0e6f88b
michael tamsil <michaeltamsil@gmail.com>
2018-12-27 11:31:48 +0700

    Updates

---
+++ precache-manifest.09a1051c87313063d95cc9f43abde332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "01be7db8e6a8ead62bf1",
+    "url": "/react-1/static/js/runtime~main.01be7db8.js"
+  },
+  {
+    "revision": "1b890dbde3e13e575e64",
+    "url": "/react-1/static/js/main.1b890dbd.chunk.js"
+  },
+  {
+    "revision": "74e40883b15eb825009b",
+    "url": "/react-1/static/js/1.74e40883.chunk.js"
+  },
+  {
+    "revision": "1b890dbde3e13e575e64",
+    "url": "/react-1/static/css/main.776de7e2.chunk.css"
+  },
+  {
+    "revision": "74e40883b15eb825009b",
+    "url": "/react-1/static/css/1.79b016c1.chunk.css"
+  },
+  {
+    "revision": "540c95253a5e8175afa007a8a52cef9f",
+    "url": "/react-1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.530f84b4340cec1a9036f3ead8eeb528.js"
+  "/react-1/precache-manifest.09a1051c87313063d95cc9f43abde332.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
